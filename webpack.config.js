b4ec572d546b1827f939835aed2b9bb4ebb67392
Darhi <isaeva-mariya@yandex.ru>
2020-03-03 23:13:54 +0300

    added webpack and babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,25 @@
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
     mode: "development",
 
     module: {
         rules: [
+          {
             test: /\.js$/,
-            use: [
-                {
-
-                }
-            ]
+            exclude: /node_modules/,
+            loader: "babel-loader"
+          }
         ]
+    },
+
+    plugins: [
+      new HTMLWebpackPlugin({
+        template: "public/index.html"
+      })
+    ],
+
+    devServer: {
+      open: true
     }
 };
