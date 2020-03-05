a0315d927ff110215eb2bf434c68d6a9c335d848
Darhi <isaeva-mariya@yandex.ru>
2020-03-05 22:14:32 +0300

    added login form

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,16 +1,27 @@
 import React from "react";
+import Container from '@material-ui/core/Container';
 import Paper from '@material-ui/core/Paper';
-import TextField from '@material-ui/core/TextField'
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    marginBottom: theme.spacing(1)
+  }
+}))
 
 const Login = () => {
+  const classes = useStyles();
+
   return (
-    <>
-    {"Login"}
-    <Paper elevation={3}>
-      <TextField type="email" />
-      <TextField type="password" />
-    </Paper>
-    </>
+    <Container maxWidth="sm">
+      <Paper elevation={3}>
+        <Container maxWidth="xs" style={{paddingTop: "20px", paddingBottom: "40px"}}>
+          <TextField className={classes.root} type="email" label="Email" fullWidth/>
+          <TextField className={classes.root} type="password" label="Password" fullWidth/>
+        </Container>
+      </Paper>
+    </Container>
   )
 }
 
